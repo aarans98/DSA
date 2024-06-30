89bca12cba464acb990ac5ce992588b532d7b8d9
ananda.aransa <ananda.aransa@gopay.co.id>
2024-07-01 05:55:32 +0700

    Adding function how to count subarray and subsequences of an array

---
+++ src/main/java/com/example/dsa/array/Array.java
@@ -0,0 +1,20 @@
+package com.example.dsa.array;
+
+public class Array {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int length = arr.length;
+
+        System.out.println("sub array count of arr : " + countSubarrays(length));
+        System.out.println("sub sequences count of arr : " + countSubSequences(length));
+    }
+
+    public static int countSubarrays(int n) {
+        return ((n)*(n+1))/2;
+    }
+
+    public static int countSubSequences(int n) {
+        return (int) Math.pow(2, n);
+    }
+}

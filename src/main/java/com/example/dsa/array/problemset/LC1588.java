cae94896dffa52321153608413e1b22aac428803
ananda.aransa <ananda.aransa@gopay.co.id>
2024-07-01 22:40:15 +0700

    LC 1588 Adding optimal solution with linear time complexity

--- src/main/java/com/example/dsa/array/problemset/LC1588.java
+++ src/main/java/com/example/dsa/array/problemset/LC1588.java
@@ -5,6 +5,7 @@ public class LC1588 {
     public static void main(String[] args) {
         int[] arr = {1,4,2,5,3};
         System.out.println(sumOddLengthSubarraysBruteForce(arr));
+        System.out.println(sumOddLengthSubarraysOptimalSolution(arr));
     }
 
     public static int sumOddLengthSubarraysBruteForce(int[] arr) {
@@ -24,5 +25,20 @@ public class LC1588 {
             }
         }
         return ans;
+    }
+
+    // intuition: https://www.youtube.com/watch?v=J5IIH35EBVE&t=18s
+    public static int sumOddLengthSubarraysOptimalSolution(int[] arr) {
+        int sum = 0;
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int total = (n-i)*(i+1);
+            if (total % 2 == 0) {
+                sum += arr[i]*(total/2);
+            } else {
+                sum += arr[i]*(1+total/2);
+            }
+        }
+        return sum;
     }
 }

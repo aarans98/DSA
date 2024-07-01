6aaeba3c8cc5a172aa51db33f24b421ad8636913
Ananda Aransa <aaransa01@gmail.com>
2024-07-02 05:55:42 +0700

    Adding number class which consist of some fundamental number operations and concepts

--- src/main/java/com/example/dsa/number/Number.java
+++ src/main/java/com/example/dsa/number/Number.java
@@ -4,6 +4,10 @@ public class Number {
 
     public static void main(String[] args) {
         System.out.println(convertToBinary(35));
+        System.out.println("bit wise AND operation of 0 and 0 : " + bitWiseAnd(0,0));
+        System.out.println("bit wise AND operation of 1 and 1 : " + bitWiseAnd(1,1));
+        System.out.println("bit wise AND operation of 29 and 300 : " + bitWiseAnd(29,300));
+        System.out.println("bit wise OR operation of 5 and 3 : " + bitWiseOr(5,3));
     }
 
     /**
@@ -24,5 +28,37 @@ public class Number {
         }
 
         return Integer.parseInt(sb.reverse().toString());
+    }
+
+    /**
+     * Bitwise and operator will compare two decimal number by converting each number to binary format and perform AND operation
+     * for each bit
+     * example:
+     *   0101 (5)
+     * & 0011 (3)
+     *   ----
+     *   0001 (1)
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public static int bitWiseAnd(int num1, int num2) {
+        return num1 & num2;
+    }
+
+    /**
+     * Bitwise or operator will compare two decimal number by converting each number to binary format and perform OR operation
+     * for each bit
+     * example:
+     *   0101 (5)
+     * | 0011 (3)
+     *   ----
+     *   0111 (1)
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public static int bitWiseOr(int num1, int num2) {
+        return num1 | num2;
     }
 }
